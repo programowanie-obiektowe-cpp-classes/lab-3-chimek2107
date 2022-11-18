7a8155202f8fb8d21278047b94b08efadcc43ee1
chimek2107 <b.chimkowski@tuta.io>
2022-11-18 14:05:12 +0100

    xD

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,22 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+	unsigned int x;
+	unsigned int y = 0;
+	unsigned int z = 0;
+
+	while (y < towar)
+	{
+		Stocznia stocznia{};
+		Statek* s1 = stocznia();
+		Zaglowiec* test = dynamic_cast<Zaglowiec*>(s1);
+
+		if (test) z += 1;
+
+		x = s1->transportuj();
+		y += x;
+		delete s1;
+	}
+
+	return z;
 }
